56cb91770d1481e9085df77e9299ee872b8c5195
Mayank <mayank.ukarde@quantiphi.com>
2022-01-01 01:38:53 +0530

    Initial-portfolio page design with static data and images

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,10 @@
 const http = require('http');
 const express = require('express')
-const bodyParser  = require('body-parser');
+const bodyParser = require('body-parser');
 const constants = require('./userDefinedConstants');
 
 var GeneralUIpath = constants.GENERAL_UI_PATH;
+var testPortfolioPath = constants.PORTFOLIO_TEST_PATH;
 
 var app = express();
 
@@ -11,30 +12,40 @@ var app = express();
  * Middleware function to get all the static content
  */
 app.use(express.static(GeneralUIpath))
-
-/**
- * Middleware function
- */
+app.use(express.static(testPortfolioPath))
+    /**
+     * Middleware function
+     */
 app.use(express.urlencoded({ extended: true }))
 
 /**
  * Express way of calling static pages
  */
-app.get('/',(req,res)=>{
-    res.sendFile('index.html',{root : GeneralUIpath})
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: GeneralUIpath })
 });
 
-app.get('/login-Details',(req,res)=>{
+app.get('/login-Details', (req, res) => {
     let username = req.query.username;
     let password = req.query.userPassword;
-    console.log('Password is ',password);
-    res.send({'username':username , 'password':password});
+    console.log('Password is ', password);
+    res.send({ 'username': username, 'password': password });
 })
+
+
+/
+// Sample get call for portfolio page(This call is for testing purpose only)
+app.get("/portfolio", (req, res) => {
+    res.sendFile('portfolio.html', {
+        root: testPortfolioPath
+    })
+})
+
 
 /**
  * Server listening to 3000 port
  */
-app.listen(3000,(req,res)=>{
+app.listen(3000, (req, res) => {
     console.log("Server is Started");
 })
 
@@ -57,6 +68,3 @@ app.listen(3000,(req,res)=>{
 
 // server.listen(3000,'localhost');
 // console.log('Server is Starting');
-
-
-
